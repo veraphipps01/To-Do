3b2b02fdac69034070b2a48385816c71556af31b
Vera Phipps <85977298+veraphipps01@users.noreply.github.com>
2022-01-06 21:11:25 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function App(){
+  const [todos, setTodos] = React.useState([])
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text:text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+  return(
+    <div className="app">
+    <div className="todo-list">
+      {todos.map((todo, i) => (
+        <Todo index={i} key={i} todo={todo} remove={removeTodo}/>
+      ))}
+      <TodoForm addTodo={addTodo}/>
+      </div>
+      </div>
+    );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
